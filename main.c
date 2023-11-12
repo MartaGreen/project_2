6c9e092890e69a270be1458bed3beb1e05caa79d
mariik <m.portnykh@fxdev.sk>
2023-11-12 13:19:17 +0100

    feat(n): open dataloger file

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int n() {
+  FILE* dataloger = fopen("dataloger_V2.txt", "r");
+  if (dataloger == NULL) {
+    printf("Zaznamy neboli nacitane!\n");
+    return 1;
+  }
+
+  return 0;
+}
+
 int main() {
+  char command;
+
+  while (command != 'k') {
+    command = getchar();
+
+    if (command == 'n') n();
+  }
   return 0;
 }
